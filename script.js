e47009d140148fc779ddc89aa02f7b8f21828cd4
prog.john <joaoricardobianchini@hotmail.com>
2021-11-08 23:25:57 -0300

    Adciona modal c mais informações das pizzas

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ pizzaJson.map((item, index) =>{
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaItem.querySelector('a').addEventListener('click', (el)=>{
+        el.preventDefault(el)
+
+        aux('.pizzaWindowArea').style.opacity = 0
+        aux('.pizzaWindowArea').style.display = 'flex'
+        setTimeout(() => {
+            aux('.pizzaWindowArea').style.opacity = 1
+        }, 200);
+     });
 
     aux('.pizza-area').append( pizzaItem )
 });
