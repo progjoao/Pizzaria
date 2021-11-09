b3cd5b9829c12947eef8765e888f343e8d0240eb
prog.john <joaoricardobianchini@hotmail.com>
2021-11-08 23:07:55 -0300

    Adiciona dados da pizza

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ const auxAll = (el)=> document.querySelectorAll(el);
 pizzaJson.map((item, index) =>{
     let pizzaItem = aux('.models .pizza-item').cloneNode(true)
 
+    
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+
     aux('.pizza-area').append( pizzaItem )
 });
