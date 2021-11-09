8e0d84bbf2f764986968d87c405505da76f65b33
prog.john <joaoricardobianchini@hotmail.com>
2021-11-09 00:15:33 -0300

    Adiciona integração de dados no modal das pizzas

--- script.js
+++ script.js
@@ -6,19 +6,26 @@ pizzaJson.map((item, index) =>{
     let pizzaItem = aux('.models .pizza-item').cloneNode(true)
 
     
-    pizzaItem.querySelector('.pizza-item--img img').src = item.img
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
-    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
-    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+    pizzaItem.setAttribute('data-key', index);
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('a').addEventListener('click', (el)=>{
-        el.preventDefault(el)
+        el.preventDefault(el);
+        let key = el.target.closest('.pizza-item').getAttribute('data-key')
+        // console.log(pizzaJson[key])
 
-        aux('.pizzaWindowArea').style.opacity = 0
-        aux('.pizzaWindowArea').style.display = 'flex'
+        aux('.pizzaBig img').src = pizzaJson[key].img
+        aux('.pizzaInfo h1').innerHTML = pizzaJson[key].name
+        aux('.pizzaInfo--desc').innerHTML = pizzaJson[key].description
+
+        aux('.pizzaWindowArea').style.opacity = 0;
+        aux('.pizzaWindowArea').style.display = 'flex';
         setTimeout(() => {
-            aux('.pizzaWindowArea').style.opacity = 1
+            aux('.pizzaWindowArea').style.opacity = 1;
         }, 200);
      });
 
-    aux('.pizza-area').append( pizzaItem )
+    aux('.pizza-area').append( pizzaItem );
 });
